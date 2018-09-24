3a1561b7d913ed99bbfd7d27651d194bbb29a563
todd.smyth <Todd.Smyth@leapdev.io>
2018-09-24 16:32:34 +1000

    attempt service worker again

---
+++ js/sw.js
@@ -0,0 +1,25 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('wander').then(function (cache) {
+            return cache.addAll([
+                '../',
+                '../index.html',
+                '../images/',
+                '../css/style.css',
+                '../css/bootstrap.css',
+                '../fonts/DancingScript-Regular.ttf',
+                '../font-awesome',
+                '../js/main.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+
+    console.log(event.request.url);
+
+});
