08b6d3ebeeb793fe08910f4277fd3f9be5cdea11
todd.smyth <Todd.Smyth@leapdev.io>
2018-10-09 15:38:20 +1100

    testing toddwritescode.com v2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
+function closeMenu(activeCard) {
+    // closes menu and adds active-card class to relevant anchors / cards
+    document.getElementById("menu-toggle").checked = false;
+    document.getElementsByClassName("active-card")[0].classList.remove("active-card");
+    if (Array.isArray(activeCard)) {
+        for (i = 0; i < activeCard.length; i++) {
+            document.getElementById(activeCard[i]).classList.add("active-card");
+        }
+    } else {
+        document.getElementById(activeCard).classList.add("active-card");
+    }
 }
