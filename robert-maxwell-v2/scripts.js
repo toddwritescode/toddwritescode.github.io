c6dc3ab576ba5f43e286ec44671354be05cf6fa4
toddwritescode <43469150+toddwritescode@users.noreply.github.com>
2019-08-15 13:29:28 +1000

    Version 2 without responsive design

--- robert-maxwell-v2/scripts.js
+++ robert-maxwell-v2/scripts.js
@@ -1,4 +1,64 @@
-// DOCUMENT LOADED LISTENER
-document.addEventListener("DOMContentLoaded", function(event) { 
+// --------------------------- DOCUMENT LOADED LISTENER
+document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('loader').classList.add('hide-loader');
-  });
+});
+
+// --------------------------- FIX SCROLL BUG
+
+function activateScroll() {
+    if (checkVisible(document.getElementById('home-section'))) {
+        console.log('home is visible');
+        document.getElementById('home-section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (checkVisible(document.getElementById('session-section'))) {
+        console.log('session is visible');
+        document.getElementById('sessions-section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (checkVisible(document.getElementById('pricing-section'))) {
+        console.log('pricing is visible');
+        document.getElementById('pricing-section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (checkVisible(document.getElementById('about-section'))) {
+        console.log('about is visible');
+        document.getElementById('about-section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (checkVisible(document.getElementById('contact-section'))) {
+        console.log('contact is visible');
+        document.getElementById('contact-section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+        console.log('at the top');
+        document.getElementById('introduction-section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
+
+function checkVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+// --------------------------- SESSIONS SLIDESHOW
+
+function nextSlide() {
+    var slides = document.getElementsByClassName('session-slide');
+    var currentActiveSlideIndex = 0; // by default
+    for (var i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('active-slide')) {
+            currentActiveSlideIndex = i;
+            slides[i].classList.remove('active-slide');
+        }
+    }
+    var newActiveSlideIndex = currentActiveSlideIndex === slides.length - 1 ? 0 : ++currentActiveSlideIndex;
+    // console.log('new active slide ', newActiveSlideIndex);
+    slides[newActiveSlideIndex].classList.add('active-slide');
+}
+
+function previousSlide() {
+    var slides = document.getElementsByClassName('session-slide');
+    var currentActiveSlideIndex = 0; // by default
+    for (var i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('active-slide')) {
+            currentActiveSlideIndex = i;
+            slides[i].classList.remove('active-slide');
+        }
+    }
+    var newActiveSlideIndex = currentActiveSlideIndex === 0 ? slides.length - 1 : --currentActiveSlideIndex;
+    // console.log('new active slide ', newActiveSlideIndex);
+    slides[newActiveSlideIndex].classList.add('active-slide');
+}
